efa183eea14d988c77100db38c62423161211bc4
mohammedAmeenM <amiameen507@gmail.com>
2026-03-10 22:37:16 +0530

    edits

--- src/users/controllers/authcontroller.js
+++ src/users/controllers/authcontroller.js
@@ -1,35 +1,30 @@
-const userService=require("../services/authservice")
+const userService = require("../services/authservice");
 
-
-const signup = async (req,res) => {
-    try {
-        const user = await userService.createUser(req.body)
-        res.status(201).json({
-            message:"User Created Successfully",
-            data:user
-        })
-    } catch (err) {
-        res.status(400).json({
-            message:err.message
-        })
-
-        
-    }
-
-}
-const login = async (req,res) => {
-    try{
-        const {email,password}=req.body
-        const user = await userService.loginUser(email,password)
-        res.status(200).json({message:"user login successful"})
-    } catch (err) {
-        res.status(400).json({
-            message:err.message
-        })
-
-    }
-}
-module.exports={
-    signup,
-    login
-}
+const signup = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json({
+      message: "User Created Successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userService.loginUser(email, password);
+    res.status(200).json({ message: "user login successful" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+};
+module.exports = {
+  signup,
+  login,
+};
