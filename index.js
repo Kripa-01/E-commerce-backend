1729b4b31335d549641f1d18386b464d7a148774
Kripa-01 <kripashaji333@gmail.com>
2026-03-15 22:55:50 +0530

    DB Connection Update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const express = require("express");
-const authroutes = require("./src/users/routers/authroutes")
+const authroutes = require("./src/users/routers/authroutes");
+const connectDB = require("./src/config/db");
 const app = express();
 const PORT = 5000;
-
+connectDB()
 app.use(express.json())
 app.use("/api/users", authroutes)
 
